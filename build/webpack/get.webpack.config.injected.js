fe4dd12b5628f6732441f9e26b0fefb8ca939210
albert patterson <apatterson189@gmail.com>
2022-06-04 18:00:28 -0500

    add license notice

--- build/webpack/get.webpack.config.injected.js
+++ build/webpack/get.webpack.config.injected.js
@@ -1,3 +1,19 @@
+/**
+ * @file
+ * @author Albert Patterson <albert.patterson.code@gmail.com>
+ * @see [Linkedin]{@link https://www.linkedin.com/in/apattersoncmu/}
+ * @see [Github]{@link https://github.com/albertpatterson}
+ * @see [npm]{@link https://www.npmjs.com/~apatterson189}
+ * @see [Youtube]{@link https://www.youtube.com/channel/UCrECEffgWKBMCvn5tar9bYw}
+ * @see [Medium]{@link https://medium.com/@albert.patterson.code}
+ *
+ * Free software under the GPLv3 licence. Permissions of this strong copyleft
+ * license are conditioned on making available complete source code of
+ * licensed works and modifications, which include larger works using a
+ * licensed work, under the same license. Copyright and license notices must
+ * be preserved. Contributors provide an express grant of patent rights.
+ */
+
 import path from 'path';
 import { getConfig as getScriptConfig } from './get.webpack.config.srcipt.js';
 import { fileURLToPath } from 'url';
